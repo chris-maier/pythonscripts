372efabe21c9d13be181dd4984e1505d11f63e04
Chris Maier <chris@chris-maier.com>
2017-05-16 20:41:15 +0200

    recursive parse function developed; print lists

--- webcrawler.py
+++ webcrawler.py
@@ -3,22 +3,31 @@ import requests
 import bs4
 import urlparse
 
-# INPUT_URL = 'http://www.duerrdental.com'
-INPUT_URL = 'http://www.bitshift-dynamics.de/'
+INPUT_URL = 'http://www.duerrdental.com'
+# INPUT_URL = 'http://www.bitshift-dynamics.de/'
+# INPUT_URL = 'http://chris-maier.com'
 
 class Page:
     def __init__(self, URL):
-        self.URL = URL
+        # self.URL = URL
         self.BASE_URL = urlparse.urlparse(URL).netloc
         self.mail = set()
         self.httpList = set()
         self.httpExtList = set()
+        self.count = 0
 
     def parse(self, urlList):
         for link in urlList:
-            if link is not in self.httpList:
-                print link
+            if not (link in self.httpList):
+                s = self.getUrlSchemeList(link)
+                l = self.getUrl(s)
+                # obtain external URL's
+                self.httpExtList.update(self.getExtUrl(s))
+                # obtain Email addresses
+                self.mail.update(self.getEmail(s))
+                # print "Obtained links: " + str(l)
                 self.httpList.add(link)
+                self.parse(l)
 
 
     def getUrlSchemeList(self, URL):
@@ -29,7 +38,7 @@ class Page:
         try:
             res.raise_for_status()
         except Exception as exc:
-            print('%s' % (exc))
+            print('Execption: %s' % (exc))
 
         # parse HTML
         s = bs4.BeautifulSoup(res.text, "lxml")
@@ -48,26 +57,67 @@ class Page:
         return scheme
 
     def getEmail(self, schemeList):
+        l = set()
         for scheme in schemeList:
             if scheme.scheme == 'mailto':
-                self.mail.add(scheme.path)
+                l.add(urlparse.urlunparse(scheme))
+                # self.mail.add(scheme.path)
+        return l
 
     def getUrl(self, schemeList):
+        l = set()
         for scheme in schemeList:
-            if (scheme.netloc == BASE_URL or scheme.netloc == '') and \
-               (scheme.scheme == 'http' or scheme.scheme == 'https'):
-                self.httpList.add(scheme.path)
+            # print "Scheme: " + str(scheme)
+            if (scheme.netloc == self.BASE_URL or scheme.netloc == ''):
+                # urlunparse helps to strip the URL
+                l.add(urlparse.urlunparse(scheme))
+        return l
 
     def getExtUrl(self, schemeList):
+        l = set()
         for scheme in schemeList:
-            if scheme.netloc != BASE_URL and (scheme.scheme == 'http' or scheme.scheme == 'https'):
-                self.httpExtList.add(scheme.netloc + scheme.path)
+            if scheme.netloc != self.BASE_URL and (scheme.scheme == 'http' or scheme.scheme == 'https'):
+                l.add(urlparse.urlunparse(scheme))
+        return l
+
+    def printEmails(self):
+        print "Mail Addresses:"
+        self.mail = sorted(self.mail)
+        for h in self.mail:
+            print h
+        print ""
+
+    def printVisitedLinks(self):
+        print "Visited HTTP Links:"
+        self.httpList = sorted(self.httpList)
+        for h in self.httpList:
+            print h
+        print ""
+
+    def printExtLinks(self):
+        print "External HTTP Links:"
+        self.httpExtList = sorted(self.httpExtList)
+        for h in self.httpExtList:
+            print h
+        print ""
+
 
 def main():
     p = Page(INPUT_URL)
-    p.parseURL()
-    print p.mail
-    print p.http
+    linklist = [INPUT_URL]
+    p.parse(linklist)
+
+    p.printVisitedLinks()
+    p.printExtLinks()
+    p.printEmails()
+
+    # s = p.getUrlSchemeList(INPUT_URL)
+    # links = p.getUrl(s)
+    # print links
+
+    # p.parse()
+    # print p.mail
+    # print p.http
 
 if __name__ == "__main__":
     main()
